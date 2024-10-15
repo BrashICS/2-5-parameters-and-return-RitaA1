78d5ae641e43be566a766d6bed3ba157f6dc3671
Rita <rita.anierobi@stu.ocsb.ca>
2024-10-15 15:08:13 +0000

    2

--- main.js
+++ main.js
@@ -11,3 +11,53 @@
 
 'use strict';
 
+
+
+//part one
+function to_fahrenheit(celsius) {
+    let fahrenheit = Math.round((celsius*9/5) + 32)
+    return fahrenheit
+}
+
+//part two
+function to_celsius(fahrenheit) {
+    let celsius = Math.round((fahrenheit - 32) * 5/9)
+    return celsius
+}
+
+//part three
+function roll_dice(qty, sides) {
+    let randInt(qty, sides) 
+        let rnd = 
+        return rnd;
+}
+
+
+
+//part four 
+function area_circle(radius) {
+    let area = Math.PI*radius**2
+    return area
+}
+
+function cylinder_volume(radius, height) {
+    let area = Math.PI*radius**2*height
+    return area
+}
+
+
+function percent(score, total) {
+    let p = score/total
+    console.log(`${Math.round(p)*100}%`)
+    return p
+}
+
+function cylinder_volume(radius, height) {
+    let area = Math.PI*radius**2*height
+    return area
+}
+
+function cylinder_volume(radius, height) {
+    let area = Math.PI*radius**2*height
+    return area
+}
